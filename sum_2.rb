7d5f59cfc24ffbc943bc368a7530685802188a60
Lee Roberts <leejamesroberts@gmail.com>
2015-12-04 09:45:47 -0500

    code and pseudo for adding two variables

---
+++ sum_2.rb
@@ -0,0 +1,17 @@
+puts "enter first number"
+a = gets.chomp.to_i
+puts "enter second number"
+b = gets.chomp.to_i
+
+def add(a, b)
+  puts "here is the total #{a+b}"
+end
+  
+add(a, b)
+
+
+
+
+
+
+
